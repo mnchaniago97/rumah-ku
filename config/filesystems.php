e249ac2678c2b324058e6f5d9e84b130b4373e03
MN Chaniago <mnurchaniago97@gmail.com>
2026-02-17 16:05:57 +0700

    fix: use public_path() for automatic public directory detection

--- config/filesystems.php
+++ config/filesystems.php
@@ -49,7 +49,7 @@ return [
 
         'uploads' => [
             'driver' => 'local',
-            'root' => env('UPLOADS_ROOT', public_path('storage')),
+            'root' => public_path('storage'),
             'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
             'visibility' => 'public',
             'throw' => false,
